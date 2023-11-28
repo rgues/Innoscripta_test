9e377648828dadfacb014c97b10b5688b80dfe76
Rodrigues Pascal Kamen Ntchabet <rgueskamen@gmail.com>
2023-11-28 21:03:17 +0100

    Updated with lastest version

--- frontend/src/types/CreateAccountRequest.ts
+++
@@ -1,6 +0,0 @@
-export interface CreateAccountRequest {
-    name: string;
-    email: string;
-    password: string;
-    password_confirmation: string;
-}
--- frontend/src/types/User.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
-    created_at: Date;
-    updated_at: Date;
-}
